1f2c34b4527c2c6c546d9074c6ff098c02be9e71
rafaelChelo <rafael@chelofinance.io>
2023-07-21 10:44:42 -0400

    feat: circuits

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-circom";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  circom: {
+    inputBasePath: "./circuits",
+    ptau: "https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau",
+    circuits: [
+      {
+        name: "crop",
+      },
+      //{
+      //name: "grayscale",
+      //},
+      //{
+      //name: "resize",
+      //},
+    ],
+  },
 };
 
 export default config;
