6f890836407747c4711548fe6375181dbf8618ab
rafaelChelo <rafael@chelofinance.io>
2023-07-22 04:24:58 -0400

    feat: first circuit and proof

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,36 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@openzeppelin/hardhat-upgrades";
+import {HardhatUserConfig} from "hardhat/config";
+//import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
 import "./tasks";
 
 require("dotenv").config();
-import "hardhat-circom";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     mumbai: {
-      url: process.env.MUMBAI_URL as string || "",
-      accounts: [
-        process.env.PK as string || ""
-      ]
-    }
-  }
-  circom: {
-    inputBasePath: "./circuits",
-    ptau: "https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau",
-    circuits: [
-      {
-        name: "crop",
-      },
-      //{
-      //name: "grayscale",
-      //},
-      //{
-      //name: "resize",
-      //},
-    ],
+      url: (process.env.MUMBAI_URL as string) || "",
+      //accounts: [(process.env.PK as string) || ""],
+    },
   },
 };
 
