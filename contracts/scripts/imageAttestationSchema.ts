eac0f591157b1b54e142f782cbe91680f969a680
rafaelChelo <rafael@chelofinance.io>
2023-07-22 08:25:39 -0400

    feat: eas

---
+++ contracts/scripts/imageAttestationSchema.ts
@@ -0,0 +1,19 @@
+import {ZeroAddress} from "ethers";
+import {ethers} from "hardhat";
+
+const createSchema = async () => {
+  //const [signer] = await ethers.getSigners();
+  //const schemaRegistryContractAddress = "0x5d29f5Bd870815147Bd008FFfbDdfE269C25E094";
+  ////const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
+  //schemaRegistry.connect(signer);
+  //const schema = "bytes32 imageHash, bytes32 hashSignature, string urlImage";
+  //const resolverAddress = ZeroAddress;
+  //const revocable = true;
+  //const transaction = await schemaRegistry.register({
+  //schema,
+  //resolverAddress,
+  //revocable,
+  //});
+  //// Optional: Wait for transaction to be validated
+  //await transaction.wait();
+};
