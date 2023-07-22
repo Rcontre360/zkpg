3259f1a1f08a588da4900064ff9dabb13f0afb99
gpablo6 <gpablo6@outlook.com>
2023-07-23 01:33:03 +0200

    Update onboarding with new index style

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
-import {PhotographIcon, DocumentAddIcon, CheckCircleIcon} from "@heroicons/react/solid"; // Importing icons
+import ConditionalButton from "@shared/components/common/ConditionalButton";
+import {PhotographIcon,
+  DocumentAddIcon, CheckCircleIcon,
+  UserIcon} from "@heroicons/react/solid"; // Importing icons
 
 export default function Home() {
   const cardData = [
@@ -16,7 +19,7 @@ export default function Home() {
       description:
         "Prove to the world that an image has been passed through several modifications.",
       icon: <DocumentAddIcon className="h-16 w-16 mb-4" />,
-      bgColor: "",
+      bgColor: "bg-blue-800 hover:bg-blue-900",
     },
     {
       href: "/verify",
@@ -24,6 +27,14 @@ export default function Home() {
       description: "Verify that an image belongs to a criptographically verified real image.",
       icon: <CheckCircleIcon className="h-16 w-16 mb-4" />,
       bgColor: "bg-violet-800 hover:bg-violet-800",
+    },
+    {
+      href: "/onboard",
+      title: "Onboarding",
+      description: "Start your journey here if you haven't registered yet.",
+      icon: <UserIcon className="h-16 w-16 mb-4" />,
+      bgColor: "bg-blue-800 hover:bg-blue-900",
+      conditional: true,
     },
   ];
 
@@ -34,19 +45,23 @@ export default function Home() {
       </header>
 
       <div className="flex gap-8 justify-center mt-8">
-        {cardData.map((card, idx) => (
-          <div
-            key={idx}
-            className={`flex flex-col items-center p-8 border border-white hover:border-opacity-60 rounded-lg transform transition-transform duration-200 hover:scale-105 ${card.bgColor} w-1/3`}
-          >
-            {card.icon}
-            <h2 className="text-xl font-bold mb-2">{card.title}</h2>
-            <p className="text-center">{card.description}</p>
-            <Link href={card.href}>
-              <a className="mt-4 text-white underline">Go to {card.title}</a>
-            </Link>
-          </div>
-        ))}
+        {cardData.map((card, idx) => 
+          card.conditional 
+          ? <ConditionalButton key={idx} card={card} /> 
+          : (
+            <div
+              key={idx}
+              className={`flex flex-col items-center p-8 border border-white hover:border-opacity-60 rounded-lg transform transition-transform duration-200 hover:scale-105 ${card.bgColor} w-1/3`}
+            >
+              {card.icon}
+              <h2 className="text-xl font-bold mb-2">{card.title}</h2>
+              <p className="text-center">{card.description}</p>
+              <Link href={card.href}>
+                <a className="mt-4 text-white underline">Go to {card.title}</a>
+              </Link>
+            </div>
+          )
+        )}
       </div>
     </main>
   );
