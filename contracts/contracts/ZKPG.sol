2d616ca8a39d721a45fc7959b9bfa7bc8161d65f
rafaelChelo <rafael@chelofinance.io>
2023-07-22 14:50:45 -0400

    feat: zkpg verifier with plonk proof

--- contracts/contracts/ZKPG.sol
+++ contracts/contracts/ZKPG.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.19;
 import "./verifiers/ZKPVerifier.sol";
 import "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
 
-contract AttestHelper is 
-    ZKPVerifier
-{
-    event SuccessfulProof (
+interface PlonkVerifier {
+    function verifyProof(
+        uint256[24] calldata _proof,
+        uint256[1] calldata _pubSignals
+    ) external view returns (bool);
+}
+
+contract ZKPG is ZKPVerifier {
+    event SuccessfulProof(
         uint256 requestId,
         uint256 challengeIndex,
         address indexed sender,
@@ -15,30 +20,28 @@ contract AttestHelper is
     );
 
     uint64 public constant TRANSFER_REQUEST_ID = 1;
+    bool public mock;
     IEAS eas;
 
     mapping(uint256 => address) public idToAddress;
     mapping(address => uint256) public addressToId;
 
-    constructor (
-        address _easAddress
-    ) {
+    constructor(address _easAddress) {
         eas = IEAS(_easAddress);
     }
 
+    function updateMock(bool _mock) public {
+        mock = _mock;
+    }
+
     function _beforeProofSubmit(
-        uint64, /* requestId */
+        uint64 /* requestId */,
         uint256[] memory inputs,
         ICircuitValidator validator
     ) internal view override {
-        // check that the challenge input of the proof is equal to the msg.sender 
-        address addr = GenesisUtils.int256ToAddress(
-            inputs[validator.getChallengeInputIndex()]
-        );
-        require(
-            _msgSender() == addr,
-            "address in the proof is not a sender address"
-        );
+        // check that the challenge input of the proof is equal to the msg.sender
+        address addr = GenesisUtils.int256ToAddress(inputs[validator.getChallengeInputIndex()]);
+        require(_msgSender() == addr, "address in the proof is not a sender address");
     }
 
     function _afterProofSubmit(
@@ -57,13 +60,11 @@ contract AttestHelper is
         idToAddress[id] = sender;
     }
 
-    function attestWrapper(
+    function publishAttestation(
         AttestationRequest calldata request
-    ) external payable returns (
-        bool
-    ) {
+    ) external payable returns (bool) {
         require(
-            proofs[msg.sender][TRANSFER_REQUEST_ID] == true,
+            proofs[msg.sender][TRANSFER_REQUEST_ID] == true || mock,
             "only identities who provided proof are allowed to be registered."
         );
 
@@ -72,4 +73,17 @@ contract AttestHelper is
         return true;
     }
 
+    function modificationAttestation(
+        AttestationRequest calldata request,
+        PlonkVerifier verifier,
+        uint256[24] calldata proof,
+        uint256[1] calldata pubSignals
+    ) external payable returns (bool) {
+        bool isValidModification = verifier.verifyProof(proof, pubSignals);
+        require(isValidModification, "invalid image modification");
+
+        eas.attest(request);
+
+        return true;
+    }
 }
--- contracts/scripts/deployVerifier.ts
+++ contracts/scripts/deployVerifier.ts
@@ -1,40 +1,37 @@
 import path from "path";
 import fs from "fs";
-import { ethers } from "hardhat";
+import {ethers} from "hardhat";
 
 const pathOutputJson = path.join(__dirname, "./deploy_verifier_output.json");
 
 async function main() {
-    const verifierContract = "AttestHelper";
-    const easContract = "0x2cDc1DDe90125Dd4E43C61247dF8892Ee15327Ce";
-  
-    const sigValidator = "0xF2D4Eeb4d455fb673104902282Ce68B9ce4Ac450";
-    const spongePoseidonLib = "0x12d8C87A61dAa6DD31d8196187cFa37d1C647153";
-    const poseidon6Lib = "0xb588b8f07012Dc958aa90EFc7d3CF943057F17d7";
-  
-  
-    const AttestHelper = await ethers.getContractFactory(verifierContract,{
-      libraries: {
-        SpongePoseidon: spongePoseidonLib,
-        PoseidonUnit6L: poseidon6Lib
-      },
-    } );
-    const attestHelper = await AttestHelper.deploy(
-      easContract
-    );
-  
-    await attestHelper.waitForDeployment();
-    console.log("contract address:", await attestHelper.getAddress());
+  const verifierContract = "ZKPG";
+  const easContract = "0x2cDc1DDe90125Dd4E43C61247dF8892Ee15327Ce";
 
-    const outputJson = {
-      verifier: await attestHelper.getAddress(),
-      eas: easContract,
-      sigValidator: sigValidator,
-      spongePoseidonLib: spongePoseidonLib,
-      poseidon6Lib: poseidon6Lib,
-      network: process.env.HARDHAT_NETWORK,
-    };
-    fs.writeFileSync(pathOutputJson, JSON.stringify(outputJson, null, 1));
+  const sigValidator = "0xF2D4Eeb4d455fb673104902282Ce68B9ce4Ac450";
+  const spongePoseidonLib = "0x12d8C87A61dAa6DD31d8196187cFa37d1C647153";
+  const poseidon6Lib = "0xb588b8f07012Dc958aa90EFc7d3CF943057F17d7";
+
+  const ZKPG = await ethers.getContractFactory(verifierContract, {
+    libraries: {
+      SpongePoseidon: spongePoseidonLib,
+      PoseidonUnit6L: poseidon6Lib,
+    },
+  });
+  const attestHelper = await ZKPG.deploy(easContract);
+
+  await attestHelper.waitForDeployment();
+  console.log("contract address:", await attestHelper.getAddress());
+
+  const outputJson = {
+    verifier: await attestHelper.getAddress(),
+    eas: easContract,
+    sigValidator: sigValidator,
+    spongePoseidonLib: spongePoseidonLib,
+    poseidon6Lib: poseidon6Lib,
+    network: process.env.HARDHAT_NETWORK,
+  };
+  fs.writeFileSync(pathOutputJson, JSON.stringify(outputJson, null, 1));
 }
 
 // Init
@@ -45,4 +42,3 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-  
